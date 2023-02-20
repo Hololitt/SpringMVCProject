b29cc0803877d644db39b4ab116749b0fe2771e7
Nikon <109473540+Hololitt@users.noreply.github.com>
2023-02-21 00:58:18 +0100

    Update Dima.java

--- Dima.java
+++ Dima.java
@@ -1,71 +1,49 @@
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Random;
-import java.util.Scanner;
-import java.lang.reflect.Method;
-import java.util.concurrent.CountDownLatch;
+import java.util.*;
 
-// 14.02.2023
+// 21.02.2023
 public class Main {
-    static <T> void start(T t) throws InvocationTargetException, IllegalAccessException {
-        Method[] method = t.getClass().getDeclaredMethods();
-        for(Method methods : method){
-            if(methods.getName().equals("getValue")){
-                if((int)methods.invoke(t) == 0){
-                    list.remove(t);
-                    System.out.println("finish");
-                }
-            } else if(methods.getName().equals("check")){
-                methods.invoke(t);
-            }
-        }
-    }
-    public static ArrayList<Word> list = new ArrayList<>();
-    public static void addWord(String word, String translation){
-        list.add(new Word(word, translation));
-    }
-    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+    public static void main(String[] args) {
+        ArrayList<CoupleOfWords> list = new ArrayList<>();
         Random random = new Random();
-Scanner scanner = new Scanner(System.in);
-        System.out.println("how many words you want to learn?");
-        int countOfWordForLearning = scanner.nextInt();
-          for(int i = 0; i<countOfWordForLearning; i++){
-              addWord(scanner.nextLine(), scanner.nextLine());
-          }
-
-       while(!list.isEmpty()){
-           start((list.get(random.nextInt(0, list.size()))));
-       }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many words you want to learn?");
+        int countOfWords = scanner.nextInt();
+        scanner.nextLine();
+        for(int i = 0; i<countOfWords; i++){
+            System.out.print("Enter the word and translation");
+            list.add(new CoupleOfWords(scanner.nextLine(),scanner.nextLine()));
+        }
+        while(!list.isEmpty()){
+            CoupleOfWords coupleOfWords = list.get(random.nextInt(0, list.size()));
+            coupleOfWords.check(list, coupleOfWords);
+        }
         System.out.println("Program is finished");
     }
 }
-class Word {
+class CoupleOfWords {
     public final String reset = "\u001B[0m";
     public final String green = "\u001B[32m";
     public final String red = "\u001B[31m";
-    CountDownLatch countDownLatch = new CountDownLatch(5);
-    Scanner scanner = new Scanner(System.in);
+    public int counts = 5;
     private final String word;
     private final String translation;
-    Word(String word, String translate){
+    CoupleOfWords(String word, String translation){
         this.word = word;
-        this.translation = translate;
+        this.translation = translation;
     }
-    public void check(){
+    public void check(ArrayList<CoupleOfWords> list, CoupleOfWords coupleOfWords){
+        var scanner = new Scanner(System.in);
         System.out.println("write translation to word "+ word);
         if(Objects.equals(scanner.nextLine(), translation)){
             System.out.println(green+"True!"+reset);
-            countDownLatch.countDown();
+            counts--;
         }else{
             System.out.println(red+"False!"+reset);
             System.out.println("Write answer was "+translation);
         }
-    }
-    public int getValue(){
-        if(countDownLatch.getCount()==0)
-            return 0;
-        else
-            return 1;
+        if(counts == 0){
+            list.remove(coupleOfWords);
+            System.out.println("You learned this word");
+        }
     }
 }
